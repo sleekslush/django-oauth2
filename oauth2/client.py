a6bd3208cb9072843f5d115841844371a670f68c
Craig Slusher <cslush@gmail.com>
2012-01-28 10:08:54 -0800

    Consolidates the strip_dict method

--- oauth2/client.py
+++ oauth2/client.py
@@ -22,42 +22,42 @@ class OAuth2Client(object):
         return '{}?{}'.format(self._get_endpoint(path), urlencode(query_params))
 
     def get_access_token(self, code, redirect_uri=None, path='token'):
-        post_data = self._strip_dict({
+        post_data = {
             'grant_type': 'authorization_code',
             'code': code,
             'redirect_uri': redirect_uri
-            })
+            }
 
         return self._post_request(path, post_data)
 
     def get_access_token_for_owner(self, username, password, scope=None, path='token'):
-        post_data = self._strip_dict({
+        post_data = {
             'grant_type': 'password',
             'username': username,
             'password': password,
             'scope': scope
-            })
+            }
 
         return self._post_request(path, post_data)
 
     def get_access_token_for_client(self, scope=None, path='token'):
-        post_data = self._strip_dict({
+        post_data = {
             'grant_type': 'client_credentials',
             'scope': scope
-            })
+            }
 
         return self._post_request(path, post_data)
 
     def refresh_access_token(self, refresh_token, scope=None, path='token'):
-        post_data = self._strip_dict({
+        post_data = {
             'grant_type': 'refresh_token',
             'scope': scope
-            })
+            }
 
         return self._post_request(path, post_data)
 
     def _post_request(self, path, data):
-        response = requests.post(self._get_endpoint(path), data=data,
+        response = requests.post(self._get_endpoint(path), data=self._strip_dict(data),
                 auth=(self.client_id, self.client_secret) if self.use_basic_auth else None)
 
         return json.loads(response.text)
