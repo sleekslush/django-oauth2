32b8915ceee8f7255f1abc27598af1d1dd14e4eb
Craig Slusher <cslush@gmail.com>
2012-02-19 22:11:45 -0800

    the goods

--- oauth2/forms.py
+++ oauth2/forms.py
@@ -4,3 +4,4 @@ from oauth2.models import ClientApplication
 class ClientApplicationForm(forms.ModelForm):
     class Meta:
         model = ClientApplication
+        fields = ('name', 'website_url', 'callback_url', 'description',)
